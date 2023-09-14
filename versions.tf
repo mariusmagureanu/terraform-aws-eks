1e6b6a48f70ddeefb2aa904bf896b635cb3b6e64
Marius Magureanu <marius.magureanu@schibsted.com>
2023-09-14 13:43:14 +0200

    Bump the kubernetes provider version requirement

--- versions.tf
+++ versions.tf
@@ -12,7 +12,7 @@ terraform {
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = ">= 2.10"
+      version = ">= 2.13"
     }
     time = {
       source  = "hashicorp/time"
